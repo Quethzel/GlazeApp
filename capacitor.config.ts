1aa099fe26dcb181d57eb1cbfbad10d467d66d3e
Quethzel <quethzel@gmail.com>
2025-03-04 17:26:13 -0600

    add capacitor.config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.clayapps.glazeapp',
+  appName: 'GlazeApp',
+  webDir: 'www'
+};
+
+export default config;
